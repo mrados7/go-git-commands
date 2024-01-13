b3a03da9423777af5e0c2de73030491d76a6aba6
Marko Rados <marko.rados7@gmail.com>
2024-01-13 14:05:48 +0100

    change checkout command to have list of branchTypes and jiraTicket selections

---
+++ cmd/checkout/config.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/charmbracelet/bubbles/list"
+	"os"
+	"path/filepath"
+)
+
+type branchType struct {
+	T string `json:"type"`
+	D string `json:"description"`
+}
+
+type jiraBoard struct {
+	Name string `json:"name"`
+	D    string `json:"description"`
+}
+
+type config struct {
+	BranchTypes []branchType `json:"branchTypes"`
+	JiraBoards  []jiraBoard  `json:"jiraBoards"`
+}
+
+const configFile = ".git-branch-checkout-2.json"
+
+var defaultBranchTypes = []list.Item{
+	branchType{"FEAT", "A new feature"},
+	branchType{"FIX", "A bug fix"},
+	branchType{"IMPR", "An improvement to a feature or enhancement"},
+	branchType{"OPS", "Changes to our CI configuration files and scripts"},
+	branchType{"CHORE", "Updating grunt tasks etc; no production code change"},
+}
+
+var defaultJiraBoards = []list.Item{
+	jiraBoard{"EPD", "CPO team board"},
+	jiraBoard{"IB", "Interim billing board"},
+}
+
+func convertBranchTypes(branchTypes []branchType) []list.Item {
+	items := []list.Item{}
+	for _, branchType := range branchTypes {
+		items = append(items, branchType)
+	}
+	if len(items) == 0 {
+		return defaultBranchTypes
+	}
+	return items
+}
+
+func convertBoards(boards []jiraBoard) []list.Item {
+	items := []list.Item{}
+	for _, board := range boards {
+		items = append(items, board)
+	}
+	if len(items) == 0 {
+		return nil
+	}
+	return items
+}
+
+func loadConfigFile(path string) ([]list.Item, []list.Item, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error reading config file: %w", err)
+	}
+	var c config
+	if err := json.Unmarshal(data, &c); err != nil {
+		return nil, nil, fmt.Errorf("error parsing config file: %w", err)
+	}
+	return convertBranchTypes(c.BranchTypes), convertBoards(c.JiraBoards), nil
+}
+
+func loadConfig() ([]list.Item, []list.Item, error) {
+	basePath, err := os.UserHomeDir()
+	if err != nil {
+		return nil, nil, fmt.Errorf("error getting home dir: %w", err)
+	}
+	targetPath, err := os.Getwd()
+	if err != nil {
+		return nil, nil, fmt.Errorf("error getting current dir: %w", err)
+	}
+	for {
+		rel, _ := filepath.Rel(basePath, targetPath)
+		if rel == "." {
+			break
+		}
+		filePath := filepath.Join(targetPath, configFile)
+		if _, err := os.Open(filePath); err == nil {
+			fmt.Println("Found config file at", filePath)
+			return loadConfigFile(filePath)
+		}
+
+		targetPath += "/.."
+	}
+	fmt.Println("No config file found, using default config")
+	return defaultBranchTypes, defaultJiraBoards, nil
+}
--- cmd/checkout/main.go
+++ cmd/checkout/main.go
@@ -2,39 +2,73 @@ package main
 
 import (
 	"fmt"
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"os"
-	"os/exec"
-	"strings"
 )
+
+const (
+	defaultWidth = 80
+	listHeight   = 35
+)
+
+func (b jiraBoard) Title() string       { return b.Name }
+func (b jiraBoard) Description() string { return b.D }
+func (b jiraBoard) FilterValue() string { return b.Name }
+
+func (p branchType) Title() string       { return p.T }
+func (p branchType) Description() string { return p.D }
+func (p branchType) FilterValue() string { return p.T }
 
 var (
 	focusedStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#6FD0FB"))
-	branchTypes  = []string{"FEAT", "FIX", "IMPR", "OPS"}
+
+	stepTitleStyle    = lipgloss.NewStyle().Background(lipgloss.Color("#DA8BFF")).Foreground(lipgloss.Color("#000000")).Padding(0, 1)
+	listTitleBarStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#000000")).Padding(1, 0)
+	docStyle          = lipgloss.NewStyle().Margin(1, 2)
+
+	inputStyle = lipgloss.NewStyle().Margin(1, 0)
 )
 
 type model struct {
 	branchType            string
-	branchTypeCursor      int
 	ticketIdInput         textinput.Model
 	branchNameInput       textinput.Model
 	step                  int
 	placeholderBranchName string
+	// new model
+	branchTypeList list.Model
+	jiraBoardList  list.Model
+
+	selectedBranchType  string
+	selectedJiraBoard   string
+	shouldEnterTicketId bool
+	shouldEnterBranch   bool
 }
 
-func initialModel() model {
+func initialModel(branchTypes []list.Item, jiraBoards []list.Item) model {
 	m := model{
-		branchType:       branchTypes[0],
-		branchTypeCursor: 0,
+		branchTypeList: list.New(branchTypes, list.NewDefaultDelegate(), defaultWidth, listHeight),
+		jiraBoardList:  list.New(jiraBoards, list.NewDefaultDelegate(), defaultWidth, listHeight),
 	}
+
+	m.branchTypeList.Styles.Title = stepTitleStyle
+	m.branchTypeList.Styles.TitleBar = listTitleBarStyle
+	m.branchTypeList.Title = "Select branch type"
+	m.branchTypeList.SetShowStatusBar(false)
+
+	m.jiraBoardList.Styles.Title = stepTitleStyle
+	m.jiraBoardList.Styles.TitleBar = listTitleBarStyle
+	m.jiraBoardList.Title = "Select Jira board"
+	m.jiraBoardList.SetShowStatusBar(false)
 
 	// ---- ticketIdInput input -----
 	var ticketIdInput textinput.Model
 
 	ticketIdInput = textinput.New()
-	ticketIdInput.SetValue("FE-XXXX")
+	ticketIdInput.Focus()
 
 	m.ticketIdInput = ticketIdInput
 
@@ -42,7 +76,7 @@ func initialModel() model {
 	var branchNameInput textinput.Model
 
 	branchNameInput = textinput.New()
-	branchNameInput.SetValue("short-message")
+	branchNameInput.Placeholder = "short-message"
 
 	m.branchNameInput = branchNameInput
 
@@ -55,166 +89,169 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
-
-	if m.step == 0 {
-		m.ticketIdInput.Blur()
-		m.branchNameInput.Blur()
-	} else if m.step == 1 {
-		m.ticketIdInput.Focus()
-		m.branchNameInput.Blur()
-	} else if m.step == 2 {
-		m.ticketIdInput.Blur()
-		m.branchNameInput.Focus()
-	}
-
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.branchTypeList.SetSize(msg.Width-h, msg.Height-v)
+		m.jiraBoardList.SetSize(msg.Width-h, msg.Height-v)
+		return m, nil
+	}
+	switch {
+	case m.selectedBranchType == "":
+		return m.UpdateBranchTypeList(msg)
+	case m.selectedJiraBoard == "":
+		return m.UpdateJiraBoardList(msg)
+	case m.shouldEnterTicketId:
+		return m.UpdateTicketIdInput(msg)
+	case m.shouldEnterBranch:
+		return m.UpdateBranchNameInput(msg)
+	}
+	return m, cmd
+}
 
+func (m model) UpdateBranchNameInput(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, _ := docStyle.GetFrameSize()
+		m.branchNameInput.Width = msg.Width - h
+		return m, nil
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
-		case tea.KeyRight:
-			if m.step < 2 {
-				m.step++
-			}
-		case tea.KeyLeft:
-			if m.step > 0 {
-				m.step--
-			}
 		case tea.KeyEnter:
-			if m.step == 0 {
-				m.branchType = branchTypes[m.branchTypeCursor]
+			err := checkoutNewBranch(getBranchName(m))
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
 			}
-			if m.step < 2 {
-				m.step++
-			} else {
-				checkoutCommand := exec.Command("git", "checkout", "-b", fmt.Sprintf("%s", getBranchName(m)))
-				checkoutCommand.Stdout = os.Stdout
-				checkoutCommand.Stderr = os.Stderr
-
-				err := checkoutCommand.Run()
-				if err != nil {
-					fmt.Println(err)
-					//return m, tea.Quit
-				}
-
-				return m, tea.Quit
-			}
-
-		case tea.KeyUp, tea.KeyDown:
-			if m.step == 0 {
-				if msg.Type == tea.KeyUp {
-					if m.branchTypeCursor > 0 {
-						m.branchTypeCursor--
-					}
-				} else {
-					if m.branchTypeCursor < len(branchTypes)-1 {
-						m.branchTypeCursor++
-					}
-				}
-			}
+			m.shouldEnterBranch = false
+			return m, tea.Quit
 		}
 	}
 
-	m.ticketIdInput, cmd = m.ticketIdInput.Update(msg)
+	var cmd tea.Cmd
 	m.branchNameInput, cmd = m.branchNameInput.Update(msg)
 	return m, cmd
 }
 
-func (m model) View() string {
-	var b strings.Builder
-
-	b.WriteString("")
-	b.WriteString(getBranchNameWithStyle(m))
-	b.WriteRune('\n')
-	b.WriteRune('\n')
-
-	b.WriteString(getSelectedOptionTitle(m.step))
-	b.WriteRune('\n')
-
-	if m.step == 0 {
-		b.WriteString(chooseBranchTypeView(m))
-	} else if m.step == 1 {
-		m.ticketIdInput.Focus()
-		b.WriteString(m.ticketIdInput.View())
-	} else if m.step == 2 {
-		m.branchNameInput.Focus()
-		b.WriteString(m.branchNameInput.View())
+func (m model) UpdateTicketIdInput(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, _ := docStyle.GetFrameSize()
+		m.ticketIdInput.Width = msg.Width - h
+		return m, nil
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.ticketIdInput.Blur()
+			m.shouldEnterTicketId = false
+			m.shouldEnterBranch = true
+			m.branchNameInput.Focus()
+			return m, nil
+		}
 	}
 
-	return b.String()
+	var cmd tea.Cmd
+	m.ticketIdInput, cmd = m.ticketIdInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) UpdateBranchTypeList(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.branchTypeList.SetSize(msg.Width-h, msg.Height-v)
+		return m, nil
+	case tea.KeyMsg:
+		if m.branchTypeList.FilterState() == list.Filtering {
+			break
+		}
+
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter, tea.KeySpace:
+			m.selectedBranchType = m.branchTypeList.SelectedItem().(branchType).Title()
+		}
+	}
+
+	var cmd tea.Cmd
+	m.branchTypeList, cmd = m.branchTypeList.Update(msg)
+	return m, cmd
+}
+
+func (m model) UpdateJiraBoardList(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.jiraBoardList.SetSize(msg.Width-h, msg.Height-v)
+		return m, nil
+	case tea.KeyMsg:
+		if m.jiraBoardList.FilterState() == list.Filtering {
+			break
+		}
+
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter, tea.KeySpace:
+			m.selectedJiraBoard = m.jiraBoardList.SelectedItem().(jiraBoard).Title()
+			m.ticketIdInput.SetValue(m.jiraBoardList.SelectedItem().(jiraBoard).Title() + "-")
+			m.shouldEnterTicketId = true
+		}
+	}
+
+	var cmd tea.Cmd
+	m.jiraBoardList, cmd = m.jiraBoardList.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	s := ""
+
+	branchName := lipgloss.NewStyle().MarginBottom(1).Foreground(lipgloss.Color("#fcbda1")).Render(getBranchName(m))
+
+	switch {
+	case m.selectedBranchType == "":
+		s += lipgloss.JoinVertical(lipgloss.Top, branchName, m.branchTypeList.View())
+	case m.selectedJiraBoard == "":
+		s += lipgloss.JoinVertical(lipgloss.Top, branchName, m.jiraBoardList.View())
+	case m.shouldEnterTicketId:
+		title := stepTitleStyle.Render("Enter ticket id")
+		s += lipgloss.JoinVertical(lipgloss.Top, branchName, title, inputStyle.Render(m.ticketIdInput.View()))
+	case m.shouldEnterBranch:
+		title := stepTitleStyle.Render("Enter branch name")
+		s += lipgloss.JoinVertical(lipgloss.Top, branchName, title, inputStyle.Render(m.branchNameInput.View()))
+	}
+
+	return s
 }
 
 func getBranchName(m model) string {
-	return fmt.Sprintf("%s/%s/%s", m.branchType, m.ticketIdInput.Value(), m.branchNameInput.Value())
-}
-
-func getBranchNameWithStyle(m model) string {
-	var branchType string
-	if m.step == 0 {
-		branchType = focusedStyle.Render(resolveHighlightedBranchType(m.branchTypeCursor, m.branchType, m.step))
-	} else {
-		branchType = resolveHighlightedBranchType(m.branchTypeCursor, m.branchType, m.step)
-	}
-
-	var ticketId string
-	if m.step == 1 {
-		ticketId = focusedStyle.Render(m.ticketIdInput.Value())
-	} else {
-		ticketId = m.ticketIdInput.Value()
-	}
-
-	var branchName string
-	if m.step == 2 {
-		branchName = focusedStyle.Render(m.branchNameInput.Value())
-	} else {
-		branchName = m.branchNameInput.Value()
-	}
-
-	return fmt.Sprintf("%s/%s/%s", branchType, ticketId, branchName)
-}
-
-func chooseBranchTypeView(m model) string {
-	var b strings.Builder
-
-	for i := 0; i < len(branchTypes); i++ {
-		if m.branchTypeCursor == i {
-			b.WriteString(focusedStyle.Bold(true).Render("> "))
-			b.WriteString(focusedStyle.Bold(true).Render(branchTypes[i]))
-		} else {
-			b.WriteString("> ")
-			b.WriteString(branchTypes[i])
-		}
-		b.WriteString("\n")
-	}
-
-	return b.String()
-}
-
-func resolveHighlightedBranchType(branchTypeCursor int, branchType string, step int) string {
-	if step != 0 {
-		return branchType
-	}
-	return branchTypes[branchTypeCursor]
-}
-
-func getSelectedOptionTitle(selectedOption int) string {
-	switch selectedOption {
-	case 0:
-		return "Select branch type:"
-	case 1:
-		return "Enter ticket id:"
-	case 2:
-		return "Enter short message:"
-	default:
-		return ""
-	}
+	return fmt.Sprintf("%s/%s/%s", m.selectedBranchType, m.ticketIdInput.Value(), m.branchNameInput.Value())
 }
 
 func main() {
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	isGitRepo := checkIfGitRepo()
+	if !isGitRepo {
+		fail("Error: not a git repository")
+	}
+	branchTypes, jiraBoards, err := loadConfig()
+	if err != nil {
+		fail("Error: %s", err)
+	}
+
+	p := tea.NewProgram(initialModel(branchTypes, jiraBoards), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func fail(format string, args ...interface{}) {
+	_, _ = fmt.Fprintf(os.Stderr, format+"\n", args...)
+	os.Exit(1)
 }
